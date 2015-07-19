3f554a5ad972cc9996e71ea1acbb46812497d7a9
zx648383079 <648383079@qq.com>
2015-07-19 14:16:53 +0800

    上传

---
+++ 密码本/密码本/ViewModel/OpenViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 密码本.ViewModel
+{
+    public class OpenViewModel:ViewModelBase
+    {
+        
+    }
+}
---
+++ 密码本/密码本/ViewModel/ViewModelBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 密码本.ViewModel
+{
+    public abstract class ViewModelBase:INotifyPropertyChanged
+    {
+        /// <summary>
+        /// 属性改变事件
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        /// <summary>
+        /// 当属性改变的时候，调用该方法来发起一个消息，通知View中绑定了propertyName的元素做出调整
+        /// </summary>
+        /// <param name="propertyName"></param>
+        public void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler !=null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
